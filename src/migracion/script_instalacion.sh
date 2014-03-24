6e3cb9aa5ca2b673a2d8af670e68bc83dbf8ae9e
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2014-03-24 17:22:57 +0100

    Updated

--- src/migracion/script_instalacion.sh
+++ src/migracion/script_instalacion.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
 echo "Bienvenido al asistente de instalación del Cefire"
+echo "Lo primero es actualizar el equipo"
 
 sudo lliurex-upgrade
 
+echo "Ahora lo dejamos todo preparado en la carpeta personal"
+cp -r data ~/
+cp -r data-heavy ~/
 
-echo "Installing all at the Desktop folder"
-cp  -r data/* .
 
+#################################
+echo " Si hay algún deb en la carpeta de data-heavy/debs/"
+echo " se instalan en el equipo via dpkg -i"
 
+for d in $(ls -1 debs/*.deb);do 
+    echo "Instalando ${d}"
+    dpkg -i ${d}
+done
+
+#################################
+echo "Ahora se copia el .wine"
+wine notepad
+
+cp -r data/wine/* $HOME/.wine/
+
+#################################
+echo "Accesos directos"
+cp data/desktops/* $HOME/Escritorio/
